c5dc071dc79a4be82bbde2d3b397d24e1b1542f2
abhishek100788 <56859294+abhishek100788@users.noreply.github.com>
2020-04-03 20:07:11 +0530

    edited login.java file

---
+++ login.java
@@ -0,0 +1,14 @@
+   class Simple
+   {
+    public static void main(String args[])
+    {
+     
+            System. out. println("Hello Java");
+ for (int i=0; i<=10;i++)
+ {
+       println("number =" + i)
+ }
+  //      println("We want to see the difference before coomit ")
+    }
+
+    }
