d9674c26a762d591513d66d2234318ac3791a541
abhishek100788 <a.madnavat@gmail.com>
2020-04-03 22:57:00 +0530

    to master

--- login.java
+++ login.java
@@ -1,14 +1,17 @@
-   class Simple
+public class login
    {
     public static void main(String args[])
     {
      
             System. out. println("Hello Java");
- for (int i=0; i<=15;i++)
+ for (int i=10; i<=15;i++)
  {
-       println("number is  =" + i)
+       System. out.println("number is  =" + i);
  }
   //      println("We want to see the difference before coomit ")
     }
 
     }
+
+
+
